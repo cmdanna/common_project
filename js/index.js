b6757906d13facdd6ef67d55a2b615bb87aa6957
cmdanna <89875999+cmdanna@users.noreply.github.com>
2023-03-19 02:19:03 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function dropdown() {
+    console.log("In dropdown")
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+    }
+  }
+}
+
